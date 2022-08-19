8e8edbd401da7492a7e27d6839e9cfba085c4bcb
Boris Pouw <74407773+BPouw@users.noreply.github.com>
2022-08-19 10:40:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const app = require('./src/app')
 
 // the order of starting the app and connecting to the database does not matter
 // since mongoose buffers queries till there is a connection
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 
 // start the app
 const port = process.env.PORT || 80
